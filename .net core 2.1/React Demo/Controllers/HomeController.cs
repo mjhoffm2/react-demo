58b301df9e3e06ea64367023e21cb595b2a8c28d
Matt Hoffman <matt.hoffman@echelon.org>
2018-10-10 09:51:36 -0500

    fixed cshtml views

--- .net core 2.1/React Demo/Controllers/HomeController.cs
+++ .net core 2.1/React Demo/Controllers/HomeController.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
-namespace BainBenchmarking.Controllers
+namespace React_Demo.Controllers
 {
 	public class HomeController : Controller
 	{
