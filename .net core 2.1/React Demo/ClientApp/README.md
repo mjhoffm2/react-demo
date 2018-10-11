e821e5a33f9df957ad77cc7be358094f21f12f61
Matt Hoffman <matt.hoffman@echelon.org>
2018-10-11 12:32:01 -0500

    got production release working for .net core

--- .net core 2.1/React Demo/ClientApp/README.md
+++
@@ -1,5 +0,0 @@
-usage:
-
-1. npm install
-2. npm run dev
-3. open browser to localhost:3000
--- .net core 2.1/React Demo/webpack.config.js
+++ .net core 2.1/React Demo/webpack.config.js
@@ -1,47 +1,56 @@
 const path = require('path');
 const webpack = require('webpack');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-var config = (env, options) => ({
-    name: 'web',
-    mode: env.NODE_ENV === 'development' ?  'development' : 'production',
-    entry: {
-        //we configure the hot reloader as a separate entry point to the application
-        //'webpack-hot-middleware/client',
+var config = (env, options) => {
+    const isProduction = env && env.NODE_ENV && env.NODE_ENV !== 'development' ? true :
+        options && options.mode === 'production' ? true : false;
+    return {
+        name: 'web',
+        mode: isProduction ? 'production' : 'development',
+        entry: {
+            //we configure the hot reloader as a separate entry point to the application
+            //'webpack-hot-middleware/client',
 
-        client: './ClientApp/src/web/boot-client.tsx'
-    },
-    output: {
-        path: path.resolve(__dirname, './wwwroot/dist'),
-        publicPath: '/dist/',
-        filename: 'bundle.js',
-    },
-    resolve: {
-        //automatically infer '.ts' and '.tsx' when importing files
-        extensions: ['.js', '.jsx', '.ts', '.tsx']
-    },
-    module: {
-        rules: [
-            {
-                test:/\.css$/,
-                use:['style-loader','css-loader'],
-            },
-            {
-                test:/\.tsx?$/,
-                include: path.resolve(__dirname, "./ClientApp/src/web/"),
-                loader: "awesome-typescript-loader"
-            },
-            {
-                test: /\.(png|jpg|jpeg|gif|svg|ttf|otf|woff|woff2|eot)$/,
-                loader: 'url-loader?limit=25000'
-            }
-        ]
-    },
+            client: './ClientApp/boot-client.tsx'
+        },
+        output: {
+            path: path.resolve(__dirname, './wwwroot/dist'),
+            publicPath: '/dist/',
+            filename: 'bundle.js',
+        },
+        resolve: {
+            //automatically infer '.ts' and '.tsx' when importing files
+            extensions: ['.js', '.jsx', '.ts', '.tsx']
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.css$/,
+                    use: isProduction ?
+                        [MiniCssExtractPlugin.loader, 'css-loader'] :
+                        ['style-loader', 'css-loader'],
+                },
+                {
+                    test: /\.tsx?$/,
+                    include: path.resolve(__dirname, "./ClientApp/"),
+                    loader: "awesome-typescript-loader"
+                },
+                {
+                    test: /\.(png|jpg|jpeg|gif|svg|ttf|otf|woff|woff2|eot)$/,
+                    loader: 'url-loader?limit=4096'
+                }
+            ]
+        },
 
-    //see https://webpack.js.org/configuration/devtool/ for options
-    devtool: "cheap-module-eval-source-map",
+        //see https://webpack.js.org/configuration/devtool/ for options
+        devtool: isProduction ? "source-map" : "cheap-module-eval-source-map",
 
-    //plugins: [
-    //    new webpack.HotModuleReplacementPlugin()
-    //]
-});
+        plugins: isProduction ? [
+            new MiniCssExtractPlugin()
+        ] : [
+                //new webpack.HotModuleReplacementPlugin()
+            ]
+    }
+}
 module.exports = config;
