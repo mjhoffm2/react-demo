8f1a5dd26fd328e7e754325fcfef953cc35fec92
Matt Hoffman <matt.hoffman@echelon.org>
2018-10-12 11:20:18 -0500

    removed test2 project

--- test2/test2/ClientApp/definitions/definitions.ts
+++
@@ -1,8 +0,0 @@
-export * from './stateDefinitions';
-
-//helper types
-export type KeysExcept<T, K extends keyof T> = {
-    [Key in keyof T]: K extends Key ? never : Key
-}[keyof T]
-
-export type RemoveKeys<T, K extends keyof T> = Pick<T, KeysExcept<T, K>>;
